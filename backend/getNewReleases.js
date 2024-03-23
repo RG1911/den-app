708d7170adb1726eb7c13a8d0d4fda99add5ec0d
fawady2j <infamousfk@gmail.com>
2024-03-22 20:00:54 -0400

    added getNewReleases, displayed results in Browse route

---
+++ backend/getNewReleases.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+const port = 3000;
+const app = express();
+app.use(cors());
+
+const accessToken = 'BQCHTQPBg4a4gvCNPaRjbx_WJxOr822GnJpPsmAqIOIvfpJFNQYxF7fZx_d-5_HkmUCTsHn8NntjjnD4ZDyBK9ysPs-ots0QDYj-NCxeoC5zfu_uJ0g'; // Replace with your actual access token
+
+app.get('/new-releases', (req, res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/browse/new-releases',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+    .then(response => {
+        const newReleasesData = response.data.albums.items;
+        const firstSixReleases = newReleasesData.slice(0, 6); // Get the first 6 items
+        const formattedData = firstSixReleases.map(album => ({
+            name: album.name,
+            artists: album.artists.map(artist => artist.name),
+            release_date: album.release_date,
+            cover_art: album.images[0].url
+        }));
+        res.send(formattedData);
+    })
+    .catch(error => {
+        res.status(500).send(error.toString());
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
--- frontend/Pages/Browse.jsx
+++ frontend/Pages/Browse.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
-function Homepage() {
+function Browse() {
+    const [newReleases, setNewReleases] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/new-releases')
+            .then(response => response.json())
+            .then(data => setNewReleases(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
     return (
         <div>
             <h1>Welcome to DEN!</h1>
             <p>Browse music.</p>
+            <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+                {newReleases.map((release, index) => (
+                    <div key={index} style={{ margin: '10px' }}> 
+                        <img src={release.cover_art} alt={release.name} className="album-cover" />
+                        <p className="album-details">{release.name}</p>
+                        <p className="album-details" style={{ marginTop: '5px' }}>{release.artists.join(', ')}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
 
-export default Homepage;
+export default Browse;
